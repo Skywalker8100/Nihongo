b44cf2645476a7f86ff99ab790b2a42d2ab654a8
Skywalker8100 <prajapatbhavishya900@gmail.com>
2025-02-03 21:46:11 +0530

    kana box

--- app/src/main/java/com/example/nihongo/data/KanaData.kt
+++ app/src/main/java/com/example/nihongo/data/KanaData.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
@@ -286,6 +287,9 @@ fun KanaBox(kana: Kana) {
     OutlinedCard(
         elevation = CardDefaults.cardElevation(
             defaultElevation = 8.dp
+        ),
+        colors = CardDefaults.outlinedCardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
         ),
         modifier = Modifier
             .size(
